8b79dda54382ac4326d31ccdb17fdf962e0773f9
mickeey2525 <m.tachibana25@gmail.com>
2021-10-30 17:13:49 +0900

    Added Client Error

--- test_deps.ts
+++ test_deps.ts
@@ -1,1 +1,2 @@
-export { assertEquals } from "https://deno.land/std@v0.111.0/testing/asserts.ts";
+export { assertEquals } from "https://deno.land/std@0.111.0/testing/asserts.ts";
+export { delay } from "https://deno.land/std@0.113.0/async/delay.ts";
